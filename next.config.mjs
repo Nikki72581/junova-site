9228b77c7fc553d559017630095432aa285a3989
Nicole Ronchetti <nicoleronchetti@mac.com>
2025-12-08 23:13:33 -0500

    updated next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn-images-1.medium.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'miro.medium.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 export default nextConfig;
